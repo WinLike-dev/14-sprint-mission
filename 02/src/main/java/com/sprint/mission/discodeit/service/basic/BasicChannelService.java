ed8d6232477aa5ee329c84a05b8873690c4dcf46
WinLike-dev <kimseoungho0219@gmail.com>
2026-07-24 16:03:37 +0900

    feat: Basic 서비스와 CRUD Repository 계약 추가

---
+++ 02/src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
@@ -0,0 +1,47 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.ChannelType;
+import com.sprint.mission.discodeit.repository.CrudRepository;
+import com.sprint.mission.discodeit.service.ChannelService;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+public class BasicChannelService implements ChannelService {
+
+    private final CrudRepository<Channel> channelRepository;
+
+    public BasicChannelService(CrudRepository<Channel> channelRepository) {
+        this.channelRepository = Objects.requireNonNull(channelRepository);
+    }
+
+    @Override
+    public Channel createChannel(ChannelType type, String name, String description) {
+        Channel channel = new Channel(type, name, description);
+        return channelRepository.create(channel);
+    }
+
+    @Override
+    public Channel readChannel(UUID id) {
+        return channelRepository.findById(id);
+    }
+
+    @Override
+    public List<Channel> readAllChannels() {
+        return channelRepository.findAll();
+    }
+
+    @Override
+    public void updateChannel(UUID id, String name, String description) {
+        Channel channel = channelRepository.findById(id);
+        channel.update(name, description);
+        channelRepository.update(channel);
+    }
+
+    @Override
+    public void deleteChannel(UUID id) {
+        channelRepository.deleteById(id);
+    }
+}
